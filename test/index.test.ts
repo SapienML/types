0c6d7226acad940f964cb797b234bb23f5905941
junaid <junaid1460@gmail.com>
2017-10-29 10:54:19 +0530

    Update : add test code to test New types

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,9 @@
 import { suite, test, slow, timeout } from "mocha-typescript";
 import * as assert from "assert" ;
+import { Tensor } from '@types';
+// import { hello } from '../src/index';
 @suite class Test{
     @test testHello(){
-        
+        var t = new Tensor()
     }
 }
