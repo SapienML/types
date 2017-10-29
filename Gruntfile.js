5e424ba0bf63b921e88f702aa7bf04f63056c46e
junaid <junaid1460@gmail.com>
2017-10-29 09:36:47 +0530

    Update  Gruntfile.js:  registering webpack and uglify tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,18 @@
 
-var uglifyConf = {
+
+let comment = '/**\n <%= pkg.name %>:<%= pkg.version %> \n Copyright 2017 <%= pkg.author %> \n Released Under The <%= pkg.license %> License\n <%= pkg.url %> \n */';
+const uglifyConf = {
     options: {
       banner: comment
     },
     build: {
-      src: 'build/sapien.js',
-      dest: 'build/sapien.min.js'
+      src: 'dist/bundle.js',
+      dest: 'dist/bundle.min.js'
     }
+}
+
+const webpackConf = {
+    dist: require("./webpack.config.js")
 }
 
 module.exports = function(grunt) {
@@ -14,16 +20,15 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
             pkg         : grunt.file.readJSON('package.json'),
-            webpack     : browserifyConf,
-            uglify      : uglifyConf,
-            ts          : typescriptConf
+            webpack     : webpackConf,
+            uglify      : uglifyConf
         });
 
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     //grunt typescript for transpiling typescript into javascript
-    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-webpack');
     // Default task(s).
-    grunt.registerTask('default', ['ts','browserify:dist', 'uglify']);
+    grunt.registerTask('default', ['webpack', 'uglify']);
 
 };
