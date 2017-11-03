76d4241520ce279c5b43be5f5a5c18d23b05dd15
junaid <junaid1460@gmail.com>
2017-11-03 21:49:33 +0530

    reagister grunt-ts task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,24 @@
-
+const tsc =  {
+    default :{
+        src : ["./src/**/*ts"],
+        outDir : "dist",},
+    options : {
+        rootDir : "./src",
+        declaration : true,
+        sourceMap : false
+    }
+}
 
 module.exports = function(grunt) {
     require("load-grunt-tasks")(grunt);
     // Project configuration.
     grunt.initConfig({
             pkg         : grunt.file.readJSON("package.json"),
-         
+            ts : tsc
         });
 
-
+    grunt.loadNpmTasks("grunt-ts")
     // Default task(s).
-    grunt.registerTask("default", ["webpack", "uglify"]);
+    grunt.registerTask("default", ["ts"]);
 
 };
